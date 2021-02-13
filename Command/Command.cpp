58b45a2c63002bf4ed33f498cccbc582be2cc769
Lara-Tomorrow <crisper1895@gmail.com>
2021-02-13 18:27:50 +0200

    <3

--- Command/Command.cpp
+++ Command/Command.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    cout << "Poliak the best! Poliak my love!";
+    cout << "Poliak the best! Poliak my love <3!";
     special for Katya
      int x = 100;
 }
