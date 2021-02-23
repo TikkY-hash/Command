53603b0830f224edfcf4cbbe32349979911db8be
Kirill <aleks.kovalenko.1998@gmail.com>
2021-02-23 21:54:07 +0200

    second

--- Command/Command.cpp
+++ Command/Command.cpp
@@ -13,61 +13,55 @@ double Simpson(double a, double b, int c);
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-link:int chooseRepit, choosemethod;
-	double a, b, sum;
-	int n;
-	cout << " Enter your start data of functions = " ;
-	cin >> a;
-	cout << "  Enter your end data of functions = " ;
-	cin >> b;
-	cout << " Enter your coefficient = " ;
-	cin >> n;
+	int userNumber = 0;
+	do {
+		int  choosemethod;
+		double a, b, sum;
+		int n;
+		cout << " Enter your start data of functions = ";
+		cin >> a;
+		cout << "  Enter your end data of functions = ";
+		cin >> b;
+		cout << " Enter your coefficient = ";
+		cin >> n;
 
-	cout << "	IntegrationMethods \n" << endl;
-	cout << " 1) Method of Rectangles \n";
-	cout << " 2) Method of Trapezium  \n";
-	cout << " 3) Method of Simpson \n" << endl;
+		cout << "	IntegrationMethods \n" << endl;
+		cout << " 1) Method of Rectangles \n";
+		cout << " 2) Method of Trapezium  \n";
+		cout << " 3) Method of Simpson \n" << endl;
 
-uncorrect: cout << " Your chose: ";
-	cin >> choosemethod;
-	cout << endl << endl;
+	uncorrect: cout << " Your chose: ";
+		cin >> choosemethod;
+		cout << endl << endl;
 
-	switch (choosemethod) {
+		switch (choosemethod) {
 
-	case 1 : 
-		cout << "	Method of Rectangles\n" << endl << endl;
-		sum = Rectangles(a, b, n);
-		cout << sum << endl;
-		break;
+		case 1:
+			cout << "	Method of Rectangles\n" << endl << endl;
+			sum = Rectangles(a, b, n);
+			cout << sum << endl;
+			break;
 
-	case 2 : 
-		cout << "	Method of Trapezium\n" << endl << endl;
-		break;
+		case 2:
+			cout << "	Method of Trapezium\n" << endl << endl;
+			break;
 
-	case 3:
-		cout << "	Method of Simpson\n" << endl << endl;
-		break;
+		case 3:
+			cout << "	Method of Simpson\n" << endl << endl;
+			break;
 
-	default:
-		cout << " Choose the correct option\n" << endl << endl;
-		goto uncorrect;
-	}
-	
-	cout << " Start over?" << endl << endl
-		<< " 1 - Yes" << endl
-		<< " 2 - No" << endl << endl
-		<< " Your chose: ";
-	cin >> chooseRepit;
-	cout << endl << endl;
-	if (chooseRepit == 1) {
-		cout << endl;
-		goto link;
-	}
-	else
-	{
-		cout << " You have chosen to end work with the program\n" << endl << endl;
-		system("pause");
-	}
+		default:
+			cout << " Choose the correct option\n" << endl << endl;
+			goto uncorrect;
+		}
+
+		cout << " Start over?" << endl << endl
+			<< " 1 - Yes" << endl
+			<< " 2 - No" << endl << endl
+			<< " Your chose: ";
+		cin >> userNumber;
+		cout << endl << endl;
+	} while (userNumber == 1);
 }
 
 
